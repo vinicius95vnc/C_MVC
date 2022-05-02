019d4f13820c00a560a9226b57d869a096fb87a8
Vinicius G. Gonçalves <78374367+vinicius95vnc@users.noreply.github.com>
2022-05-02 14:46:28 -0300

    Adicionando Migration

---
+++ Data/Migrations/20220502172304_AdicionandoParticipantes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMVC.Data.Migrations
+{
+    public partial class AdicionandoParticipantes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participantes",
+                columns: table => new
+                {
+                    CodPar = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataCriação = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataAtualização = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participantes", x => x.CodPar);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participantes");
+        }
+    }
+}
